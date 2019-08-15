b00903da689a44d98526289ce94af4ee0f590b65
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-15 14:48:44 -0400

    Set up dummy script

--- @LNLModel/batchFunction.m
+++ @LNLModel/batchFunction.m
@@ -2,22 +2,26 @@
 % collects the mean spike waveforms from a root object
 % for a specified filename/filecode
 
-function batchFunction(index, batchname, location, outfile, test)
-
-  if ~test
-    addpath(genpath('/projectnb/hasselmogrp/hoyland/RatCatcher'))
-    addpath(genpath('/projectnb/hasselmogrp/hoyland/srinivas.gs_mtools'))
-    addpath(genpath('/projectnb/hasselmogrp/hoyland/CMBHOME'))
-    addpath(genpath('projectnb/hasselmogrp/hoyland/ln-model-of-mec-neurons'))
-  end
-
-  % acquire the filename and filecode
-  % the filecode should be the "cell number" as a 1x2 vector
-  [filename, filecode] = RatCatcher.read(location, batchname, index);
-
+% function batchFunction(index, batchname, location, outfile, test)
+%
+%   if ~test
+%     addpath(genpath('/projectnb/hasselmogrp/hoyland/RatCatcher'))
+%     addpath(genpath('/projectnb/hasselmogrp/hoyland/srinivas.gs_mtools'))
+%     addpath(genpath('/projectnb/hasselmogrp/hoyland/CMBHOME'))
+%     addpath(genpath('projectnb/hasselmogrp/hoyland/ln-model-of-mec-neurons'))
+%   end
+%
+%   % acquire the filename and filecode
+%   % the filecode should be the "cell number" as a 1x2 vector
+%   [filename, filecode] = RatCatcher.read(location, batchname, index);
+  filename = 'name';
+  filecode = [1, 1];
   % load the data
   % expect a 1x1 Session object named "root"
   load(filename);
+  root.cel = filecode;
+
+
 
   %% TODO:
   %% Do whatever needs to be done here to preprocess
@@ -29,4 +33,4 @@ function batchFunction(index, batchname, location, outfile, test)
   %   e.g. csvwrite(outfile, all_the_data);
 
 
-end % function
+% end % function
