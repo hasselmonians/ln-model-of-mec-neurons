8cdda97805885af990d226214cbb8e57da5e80d9
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-21 10:56:28 -0400

    minor fixes

--- @LNLModel/batchFunction.m
+++ @LNLModel/batchFunction.m
@@ -2,37 +2,27 @@
 % collects the mean spike waveforms from a root object
 % for a specified filename/filecode
 
-% function batchFunction(index, batchname, location, outfile, test)
-%
-%   if ~test
-%     addpath(genpath('/projectnb/hasselmogrp/hoyland/RatCatcher'))
-%     addpath(genpath('/projectnb/hasselmogrp/hoyland/srinivas.gs_mtools'))
-%     addpath(genpath('/projectnb/hasselmogrp/hoyland/CMBHOME'))
-%     addpath(genpath('projectnb/hasselmogrp/hoyland/ln-model-of-mec-neurons'))
-%   end
-%
-%   % acquire the filename and filecode
-%   % the filecode should be the "cell number" as a 1x2 vector
-%   [filename, filecode] = RatCatcher.read(location, batchname, index);
-  filename = 'name';
-  filecode = [1, 1];
+function batchFunction(index, batchname, location, outfile, test)
+
+  if ~test
+    addpath(genpath('/projectnb/hasselmogrp/hoyland/RatCatcher'))
+    addpath(genpath('/projectnb/hasselmogrp/hoyland/srinivas.gs_mtools'))
+    addpath(genpath('/projectnb/hasselmogrp/hoyland/CMBHOME'))
+    addpath(genpath('projectnb/hasselmogrp/hoyland/ln-model-of-mec-neurons'))
+  end
+
+  % acquire the filename and filecode
+  % the filecode should be the "cell number" as a 1x2 vector
+  [filename, filecode] = RatCatcher.read(location, batchname, index);
+
   % load the data
   % expect a 1x1 Session object named "root"
   load(filename);
   root.cel = filecode;
 
-
-
-  %% TODO:
-  %% Do whatever needs to be done here to preprocess
-  %   you should have the "root" object and the "filecode", so
-  %   root.cel = filecode;
-  %   and then do whatever
-  %% Run the associated functions/scripts
-  %% Save the data to a file named "outfile"
-  %   e.g. csvwrite(outfile, all_the_data);
+  Results = run_LNmodel_HD_ratCatcher(filename,filecode);
 
   outfile = [outfile(end-2:end) 'mat'];
-  save(outfile, 'var1', 'var2', 'var3')
+  save(outfile, 'Results');
 
 % end % function
