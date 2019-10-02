a06ae9afbb8661145a31470afe8538989fb26b8a
Alec Hoyland <alec.hoyland@posteo.net>
2019-10-02 14:08:23 -0400

    Add script to run Caitlin's data

---
+++ runCaitlinData.m
@@ -0,0 +1,31 @@
+r = RatCatcher;
+
+r.expID = {'Caitlin', 'A'; 'Caitlin', 'B'; 'Caitlin', 'C'; 'Caitlin', 'D', 'Caitlin', 'E'; 'Caitlin', 'F'};
+r.remotepath = '/projectnb/hasselmogrp/hoyland/LNLModel/cluster/';
+r.localpath = '/mnt/hasselmogrp/hoyland/LNLModel/cluster/';
+r.protocol = 'LNLModel';
+r.project = 'hasselmogrp';
+r.verbose = true;
+
+% load('/mnt/hasselmogrp/hoyland/data/holger/data-unmerged.mat')
+%
+% r.filenames = filenames;
+% r.filecodes = filecodes;
+
+return
+
+% batch files
+r = r.batchify;
+
+% NOTE: run the 'qsub' command on the cluster now (see output in MATLAB command prompt)
+
+return
+
+% NOTE: once the cluster finishes, run the following commands
+
+% gather files
+r = r.validate;
+dataTable = r.gather;
+dataTable = r.stitch(dataTable);
+
+return
