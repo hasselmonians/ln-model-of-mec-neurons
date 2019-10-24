23825b048b2d354a66fbe5f0283212079538eceb
Alec Hoyland <alec.hoyland@posteo.net>
2019-10-24 15:06:44 -0400

    Refactor to make it easier to read code

--- caitlin_scc/pos_map.m
+++ caitlin_scc/pos_map.m
@@ -1,23 +1,24 @@
-function [posgrid, bins] = pos_map(pos, nbins, boxSize)
+function [posgrid, bins] = pos_map(pos, nBins, boxSize)
 
 % take the histogram
 
-bins = boxSize/nbins/2:boxSize/nbins:boxSize-boxSize/nbins/2;
+boxUnit = boxSize / nBins;
+bins = boxUnit/2:boxUnit:boxSize-boxUnit/2;
 
 % store grid
-posgrid = zeros(length(pos), nbins^2);
+posgrid = zeros(length(pos), nBins^2);
 
 % loop over positions
 for idx = 1:length(pos)
-    
+
     % figure out the position index
     [~, xcoor] = min(abs(pos(idx,1)-bins));
     [~, ycoor] = min(abs(pos(idx,2)-bins));
-    
-    bin_idx = sub2ind([nbins, nbins], nbins - ycoor + 1, xcoor);
-    
+
+    bin_idx = sub2ind([nBins, nBins], nBins - ycoor + 1, xcoor);
+
     posgrid(idx, bin_idx) = 1;
-    
+
 end
 
 end
