623c26d23fc9b9111639960205fa9ad38c9538b5
Alec Hoyland <alec.hoyland@posteo.net>
2019-10-24 15:32:59 -0400

    Refactor to allow rectangular grids

--- caitlin_scc/pos_map.m
+++ caitlin_scc/pos_map.m
@@ -1,24 +1,21 @@
-function [posgrid, bins] = pos_map(pos, nBins, boxSize)
+function [posgrid, xBins, yBins] = pos_map(pos, nBins, boxSize)
 
-% take the histogram
-
+% compute the size of a grid unit
 boxUnit = boxSize / nBins;
-bins = boxUnit/2:boxUnit:boxSize-boxUnit/2;
-
+% collect the center points of all the bins
+xBins = boxUnit/2:boxUnit:xBoxSize-boxUnit/2;
+yBins = boxUnit/2:boxUnit:yBoxSize-boxUnit/2;
 % store grid
-posgrid = zeros(length(pos), nBins^2);
+posgrid = zeros(length(pos), length(xBins) * length(yBins));
 
-% loop over positions
+% compute the bin index for each data point
 for idx = 1:length(pos)
 
     % figure out the position index
-    [~, xcoor] = min(abs(pos(idx,1)-bins));
-    [~, ycoor] = min(abs(pos(idx,2)-bins));
+    [~, xcoor] = min(abs(pos(idx, 1) - xBins));
+    [~, ycoor] = min(abs(pos(idx, 2) - yBins));
 
     bin_idx = sub2ind([nBins, nBins], nBins - ycoor + 1, xcoor);
-
     posgrid(idx, bin_idx) = 1;
-
-end
 
 end
